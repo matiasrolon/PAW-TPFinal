243f8fcb80d5ee393f44118454c17ccca53c766b
Matias <matirolon96@gmail.com>
2019-06-09 21:49:27 -0300

    Redireccionamiento a la home de nuevo cuando se logea

--- mvj-reviews/resources/views/home.blade.php
+++ mvj-reviews/resources/views/home.blade.php
@@ -1,23 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+            <div class="content">
+                <div class="title m-b-md">
+                    ESTO ES LA HOME MUCHACHOS UMA DELIZIA
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+              </div>
+
+  @endsection
