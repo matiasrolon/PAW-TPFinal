9cb1b32bde08a377fbad5ea06a1a0b3ce5a09ee4
mrgrassho <mr.grassho@gmail.com>
2019-06-27 15:28:44 -0300

    Agrega correciones /home, peliculas por genero.
    + acomoda html generado por js.
    + arregla peticiones ajax onscroll

--- mvj-reviews/public/js/peliculasGenero.js
+++ mvj-reviews/public/js/peliculasGenero.js
@@ -22,10 +22,6 @@ PeliculaGenero.crearElemento = function (e, clss, inner){
 PeliculaGenero.buildGrid = function (response) {
   console.log(response);
   var resp = JSON.parse(response.responseText);
-  let b = PeliculaGenero.crearElemento("div", ["container-peliculas-populares"]);
-  let c = PeliculaGenero.crearElemento("section", ["peliculas-genero"]);
-  c.setAttribute("onscroll","PeliculaGenero.getNextChunckScroll()");
-  b.appendChild(c);
   for (var ii in resp) {
     let puntaje = (resp[ii]['puntaje']).toFixed(1);
     let d = PeliculaGenero.crearElemento("div", ["flip-card"]);
@@ -38,7 +34,7 @@ PeliculaGenero.buildGrid = function (response) {
     let j = PeliculaGenero.crearElemento("div", ["flip-card-back"]);
     let k = PeliculaGenero.crearElemento("p", [], resp[ii]['fecha_estreno']);
 
-    let l = PeliculaGenero.crearElemento("p", ["titulo-film"]);
+    let l = PeliculaGenero.crearElemento("p", ["titulo-film"], resp[ii]['titulo']);
     let m = PeliculaGenero.crearElemento("p", [], resp[ii]['sinopsis']);
     i.setAttribute("href", "/films/"+ resp[ii]['id']);
     f.appendChild(g);
@@ -47,12 +43,12 @@ PeliculaGenero.buildGrid = function (response) {
     j.appendChild(l);
     j.appendChild(m);
     i.appendChild(j);
+    e.appendChild(i);
     e.appendChild(f);
-    e.appendChild(j);
     d.appendChild(e);
-    c.append(d);
+    PeliculaGenero.containerCards.append(d);
   }
-  PeliculaGenero.container.appendChild(b);
+
 }
 
 PeliculaGenero.getNextChunck = function(){
@@ -76,9 +72,13 @@ PeliculaGenero.initialize = function (genre, category, container) {
   PeliculaGenero.category = category;
   PeliculaGenero.container = document.getElementById(container);
   PeliculaGenero.container.innerHTML = '';
-  let nom = document.querySelector(".generos li div[id='"+PeliculaGenero.genre+"+']");
+  let nom = document.querySelector(".generos li div[value='"+PeliculaGenero.genre+"']").innerText;
   let a = PeliculaGenero.crearElemento("h3", [], "Top de Peliculas de "+nom+": ");
   PeliculaGenero.container.appendChild(a);
+  let b = PeliculaGenero.crearElemento("div", ["container-peliculas-populares"]);
+  PeliculaGenero.containerCards = PeliculaGenero.crearElemento("section", ["peliculas-genero"]);
+  PeliculaGenero.containerCards.setAttribute("onscroll","PeliculaGenero.getNextChunckScroll()");
+  b.appendChild(PeliculaGenero.containerCards);
+  PeliculaGenero.container.appendChild(b);
   PeliculaGenero.getNextChunck();
-
 }
