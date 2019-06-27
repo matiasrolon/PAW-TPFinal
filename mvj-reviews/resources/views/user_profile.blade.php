f1a38ab88424fb27c6b2facc88fa8c865d9a03dd
Juan Cruz Real <juancrhcp@hotmail.com>
2019-06-27 03:36:26 -0300

    Corregido el perfil de usuario

--- mvj-reviews/resources/views/user_profile.blade.php
+++ mvj-reviews/resources/views/user_profile.blade.php
@@ -8,21 +8,22 @@
 
 @section('content')
             <div class="content">
-                <div class="title m-b-md">
+                <!-- <div class="title m-b-md"> -->
                      {{-- @guest = persona no logeada en la pagina.  (Y asi se comenta en blade) --}}
 
                     @guest
-                      <p> Estas en el perfil del usuario {{$user['username']}} </p>
+                      <!-- <p> Estas en el perfil del usuario {{$user['username']}} </p> -->
                     @else
                         @if (Auth::user()->username == $user['username'])
                           <button> EDITAR PERFIL</button>(SIN FUNCIONALIDAD)
                         @else
-                          <p> Estas en el perfil del usuario {{$user['username']}} </p>
+                          <!-- <p> Estas en el perfil del usuario {{$user['username']}} </p> -->
                         @endif
                     @endguest
 
+                    <h2 class="titulo">{{$user['username']}}</h2>
                       <div class="planilla-usuario" >
-                        <h2>Datos usuario</h2>
+                        
                         <br>
                         @if ($user['poster']==null)
                           <img class="avatar" src="{{asset('images/default_avatar.png')}}">
@@ -30,43 +31,48 @@
                           <img class="avatar" src="data:image/png;base64,{{$user['avatar']}}">
                         @endif
                         <div class="info-usuario">
-                            <label class="dato-usuario" for=""><b>Username: </b>{{$user['username']}}</label>
-                            <label class="dato-usuario" for=""><b>Rango:</b>{{$user['rango']}}</label>
-                            <label class="dato-usuario" for=""><b>Puntos:</b>{{$user['puntos']}}</label>
-                            <label class="dato-usuario" for=""><b>Nombre :</b>{{$user['nombre']}}</label>
-                            <label class="dato-usuario" for=""><b>Bio:</b>{{$user['Biografia']}}</label>
-                            <label class="dato-usuario" for=""><b>Email:</b>{{$user['email']}}</label>
+                            <h3 class="titulo-info">Informacion personal</h3>
+                            <!-- <label class="dato-usuario" for=""><b>Username: </b>{{$user['username']}}</label> -->
+                            <label class="dato-usuario" for=""><b>Nombre: </b>{{$user['nombre']}}</label>
+                            <label class="dato-usuario" for=""><b>Email: </b>{{$user['email']}}</label>
+                            <label class="dato-usuario" for=""><b>Rango: </b>{{$user['rango']}}</label>
+                            <label class="dato-usuario" for=""><b>Puntos: </b>{{$user['puntos']}}</label>
+                            <!-- <label class="dato-usuario" for=""><b>Bio:</b>{{$user['biografia']}}</label> -->
                         </div>
                         <div class="info-favoritos">
-                            <label class="titulo-favoritos"> Favoritos </label>
-                            <label class="dato-favorito" for=""><b>Genero </b>{{$user['genero_fav']}}</label>
-                            <label class="dato-favorito" for=""><b>Pelicula </b>{{$user['pelicula_fav']}}</label>
-                            <label class="dato-favorito" for=""><b>Serie </b>{{$user['serie_fav']}}</label>
+                            <h3 class="titulo-favoritos">Favoritos</h3>
+                            <label class="dato-favorito" for=""><b>Genero: </b>{{$user['genero_fav']}}</label>
+                            <label class="dato-favorito" for=""><b>Pelicula: </b>{{$user['pelicula_fav']}}</label>
+                            <label class="dato-favorito" for=""><b>Serie: </b>{{$user['serie_fav']}}</label>
                         </div>
                       </div>
-                                              <h2>Reviews Recientes </h2>
-                      <div class="container-reviews">
+                      
+                      
+                      <h2>Biografia</h2>
+                      <p>{{ $user['biografia'] }}</p>
+                      
 
+                      <h2>Reviews Recientes </h2>
                         @if (count($reviews)>0)
+                          <div class="container-reviews">
                             @foreach ($reviews as $review)
                               <div class="review-user">
                                 <a href="/films/{{$review['film_id']}}">
-                                    <div class="info-review-user">
-                                      <label class="info-review" for=""> <b> Review: </b> {{ $review['titulo'] }}</label>
-                                      <label class="info-review" for=""> <b> Puntos review: </b> {{ $review['puntaje_total'] }} </label>
-                                      <label class="info-review" for=""> <b> Pelicula: </b> {{ $review['pelicula'] }}</label>
-                                    </div>
-                                    <img class="poster" src="data:image/png;base64,{{$review['poster']}}">
+                                  <img class="poster" src="data:image/png;base64,{{$review['poster']}}">
+                                  <div class="info-review-user">
+                                    <label class="info-review" for=""> <b> Review: </b> {{ $review['titulo'] }}</label>
+                                    <label class="info-review" for=""> <b> Puntos: </b> {{ $review['puntaje_total'] }} </label>
+                                    <!-- <label class="info-review" for=""> <b> Pelicula: </b> {{ $review['pelicula'] }}</label> -->
+                                  </div>
                                 </a>
                               </div>
-                              <br>
                             @endforeach
+                          </div>
                         @else
-                            <p>Aun no hay reviews hechas para este film. </p>
+                            <p>Este critico aun no ha hecho reviews.</p>
                         @endif
-                      </div>
 
-                </div>
+                <!-- </div> -->
               </div>
 
   @endsection
