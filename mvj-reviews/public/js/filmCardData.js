914332469682e044c82e71010c13d6e7976f7f4b
Juan Cruz Real <juancrhcp@hotmail.com>
2022-09-02 00:12:04 -0300

    Home | El puntaje 0 aparece en gris en vez de rojo

--- mvj-reviews/public/js/filmCardData.js
+++ mvj-reviews/public/js/filmCardData.js
@@ -1,16 +1,17 @@
 var FilmCardData = FilmCardData || {};
 
-FilmCardData.getClasePuntaje = function(p) {
-  let cl;
-  if (p < 3.3) cl = "puntuacion-mala";
-  else if (p < 6.6) cl = "puntuacion-buena";
-  else cl = "puntuacion-muy-buena";
-  return cl;
+FilmCardData.getClasePuntaje = function (score) {
+    let cl;
+    if (score == 0) cl = "sin-puntuar";
+    else if (score < 3.3) cl = "puntuacion-mala";
+    else if (score < 6.6) cl = "puntuacion-buena";
+    else cl = "puntuacion-muy-buena";
+    return cl;
 }
 
-FilmCardData.modificarPuntajeClase = function() {
-  let array = document.querySelectorAll(".puntuacion");
-  for (let p of array) {
-    p.classList.add(FilmCardData.getClasePuntaje(p.innerHTML));
-  }
+FilmCardData.modificarPuntajeClase = function () {
+    let array = document.querySelectorAll(".puntuacion");
+    for (let score of array) {
+        score.classList.add(FilmCardData.getClasePuntaje(score.innerHTML));
+    }
 }
